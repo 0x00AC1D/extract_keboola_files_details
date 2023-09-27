41183c837567a5d3502fa5528bc38175f5ea7235
0x00AC1D <37276130+0x00AC1D@users.noreply.github.com>
2023-09-27 15:26:34 +0200

    reduce file opens

--- extract_keboola_files_details.py
+++ extract_keboola_files_details.py
@@ -29,33 +29,30 @@ with open('output.csv', 'w', newline='') as output_file:
     writer = csv.DictWriter(output_file, fieldnames=OUTPUT_FIELDS, delimiter=';')
     writer.writeheader()
 
-while True:
-    response = requests.get(URL_ENDPOINT, headers=headers, params={**params, 'offset': offset})
+    while True:
+        response = requests.get(URL_ENDPOINT, headers=headers, params={**params, 'offset': offset})
 
-    if response.status_code // 100 != 2:
-        logging.info(f'Request with offset: {offset} and limit: {limit} failed with status code: {response.status_code}')
-        logging.info(f'Failed request body: {response.text}')
+        if response.status_code // 100 != 2:
+            logging.info(f'Request with offset: {offset} and limit: {limit} failed with status code: {response.status_code}')
+            logging.info(f'Failed request body: {response.text}')
 
-        if retries >= MAX_RETRIES:
-            logging.error(f'Request attempts with offset: {offset} and limit: {limit} reached max retries: {MAX_RETRIES}')
-            raise Exception('Max request retries reached')
+            if retries >= MAX_RETRIES:
+                logging.error(f'Request attempts with offset: {offset} and limit: {limit} reached max retries: {MAX_RETRIES}')
+                raise Exception('Max request retries reached')
 
-        logging.info(f'Retrying in {RETRY_INTERVAL} seconds')
-        time.sleep(RETRY_INTERVAL)
-        retries += 1
-        continue
-    
-    if not response.json():
-        logging.info(f'Request with offset: {offset} and limit: {limit} returned no data')
-        break
+            logging.info(f'Retrying in {RETRY_INTERVAL} seconds')
+            time.sleep(RETRY_INTERVAL)
+            retries += 1
+            continue
+        
+        if not response.json():
+            logging.info(f'Request with offset: {offset} and limit: {limit} returned no data')
+            break
 
-    logging.info(f'Successfully retrieved detail for files with offset: {offset} and limit: {limit}')
-
-    with open('output.csv', 'a', newline='') as output_file:
-        writer = csv.DictWriter(output_file, fieldnames=OUTPUT_FIELDS, delimiter=';')
+        logging.info(f'Successfully retrieved detail for files with offset: {offset} and limit: {limit}')
 
         for file_detail in response.json():
             writer.writerow(file_detail)
 
-    offset += limit
-    retries = 0
+        offset += limit
+        retries = 0
